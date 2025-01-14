948ba22898dc7bac0238cc87ff912d56ac7d780d
farbod302 <f.aliakbari302@gmail.com>
2025-01-14 16:42:30 +0330

    move to https

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express")
-const cors=require("cors")
+const cors = require("cors")
+const https = require("https")
+const fs = require("fs")
 const app = express()
 app.use(cors())
 const bodyParser = require("body-parser")
 app.use(bodyParser.json())
-app.listen(4040, () => { console.log("server run on port 4040") })
-const api=require("./API")
+const api = require("./API")
+
+const conf = {
+    key: fs.readFileSync("/etc/letsencrypt/live/nutrostyle.nutrosal.com/privkey.pem"),
+    cert: fs.readFileSync("/etc/letsencrypt/live/nutrostyle.nutrosal.com/fullchain.pem")
+}
 api.init(app)
+const server=https.createServer(conf,app)
+server.listen(4015, () => { console.log("server run on port 4015") })
